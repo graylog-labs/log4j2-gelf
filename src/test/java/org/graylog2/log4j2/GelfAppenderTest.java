d4c7ba88c806fd1c64f78e6483708b00b2f283fe
Nathaniel Auvil <nathaniel_auvil@cable.comcast.com>
2015-02-08 07:59:18 -0500

    reformatted

--- src/test/java/org/graylog2/log4j2/GelfAppenderTest.java
+++ src/test/java/org/graylog2/log4j2/GelfAppenderTest.java
@@ -11,64 +11,55 @@ import org.junit.AfterClass;
 import org.junit.Test;
 
 
-public class GelfAppenderTest
-{
-	@Test
-	public void testLog()
-	{
-		final Logger logger = LogManager.getLogger( "test" );
-		logger.info( "Hello World" );
-	}
+public class GelfAppenderTest {
+    @Test
+    public void testLog() {
+        final Logger logger = LogManager.getLogger("test");
+        logger.info("Hello World");
+    }
 
 
-	@Test
-	public void testMarker()
-	{
-		final Logger logger = LogManager.getLogger( "test" );
-		final Marker parent = MarkerManager.getMarker( "PARENT" );
-		final Marker marker = MarkerManager.getMarker( "TEST" ).addParents( parent );
-		logger.info( marker, "Hello World" );
-	}
-	
-
-	@Test
-	public void testException()
-	{
-		final Logger logger = LogManager.getLogger( "test" );
-
-		try
-		{
-			throw new Exception( "Test" );
-		}
-		catch( Exception e )
-		{
-			e.fillInStackTrace();
-			logger.error( "Hello World", e );
-		}
-	}
+    @Test
+    public void testMarker() {
+        final Logger logger = LogManager.getLogger("test");
+        final Marker parent = MarkerManager.getMarker("PARENT");
+        final Marker marker = MarkerManager.getMarker("TEST").addParents(parent);
+        logger.info(marker, "Hello World");
+    }
 
 
-	@Test
-	public void testThreadContext()
-	{
-		final Logger logger = LogManager.getLogger( "test" );
+    @Test
+    public void testException() {
+        final Logger logger = LogManager.getLogger("test");
 
-		ThreadContext.push( "Message only" );
-		ThreadContext.push( "int", 1 );
-		ThreadContext.push( "int-long-string", 1, 2l, "3" );
-		ThreadContext.put( "key", "value" );
-
-		logger.info( "Hello World" );
-
-		ThreadContext.clearAll();
-
-	}
+        try {
+            throw new Exception("Test");
+        } catch (Exception e) {
+            e.fillInStackTrace();
+            logger.error("Hello World", e);
+        }
+    }
 
 
-	@AfterClass
-	public static void shutdown() throws InterruptedException
-	{
-		//need to wait to hope the underlying gelf client pushes the messages.
-		Thread.sleep( 500 );
-	}
+    @Test
+    public void testThreadContext() {
+        final Logger logger = LogManager.getLogger("test");
+
+        ThreadContext.push("Message only");
+        ThreadContext.push("int", 1);
+        ThreadContext.push("int-long-string", 1, 2l, "3");
+        ThreadContext.put("key", "value");
+
+        logger.info("Hello World");
+
+        ThreadContext.clearAll();
+
+    }
+
+
+    @AfterClass
+    public static void shutdown() throws InterruptedException {
+        //need to wait to hope the underlying gelf client pushes the messages.
+        Thread.sleep(500);
+    }
 }
